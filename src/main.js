7369d9af91618d96be8d9d1dda85feb4747a4788
TontonBanton <pancla13ulas@gmail.com>
2024-07-22 16:27:43 +0800

    CDN MaterializedCSS, FontAwesome, Jquery on index.html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router'     // Import the router
 
 // Create and mount the Vue app
 const app = createApp(App)
-app.use(router) // Install the router
+app.use(router)                   // Install the router
 app.mount('#app')
 
